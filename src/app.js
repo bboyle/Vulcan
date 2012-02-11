8279d377bc48e39f496d330fe2bea90d1f7cce4c
Ben Boyle <benjamins.boyle@gmail.com>
2012-02-12 02:08:36 +1000

    routes to view movies
    - list movies in directory (link)
    - show movie info (title + poster)
    - load a default poster

--- src/app.js
+++ src/app.js
@@ -27,10 +27,15 @@
 		app.use( express.errorHandler() );
 	});
 
+
 	// Routes
-
 	app.get( '/', routes.index );
+	app.get( '/movie/list', routes.listMovies );
+	app.get( '/movie/:title/', routes.getMovie );
+	app.get( '/movie/:title/poster', routes.getMoviePoster );
 
+
+	// start
 	app.listen( 3000 );
 	console.log( 'Express server listening on port %d in %s mode', app.address().port, app.settings.env );
 
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,14 +1,36 @@
-(function( exports ) {
+(function( exports, require ) {
 	'use strict';
 
-	/*
-	 * GET home page.
-	 */
 
+	// use for home page
 	exports.index = function( req, res ) {
 		res.render( 'index', { title: 'Express' });
 	};
 
 
+	// list all movies in folder
+	exports.listMovies = function( req, res ) {
+		var fs = require( 'fs' );
+
+		fs.readdir( 'f:/dvd/Movies', function( err, files ) {
+			res.render( 'movie/list', { title: 'Movies', files: files });
+		});
+	};
+
+
+	// get details for a movie
+	exports.getMovie = function( req, res ) {
+		var title = req.params.title;
+		res.render( 'movie/get', { title: title });
+	};
+
+
+	// get poster image for a movie
+	exports.getMoviePoster = function( req, res ) {
+		// var title = req.params.title;
+		res.sendfile( 'f:/dvd/Movies/Dirty Dancing/dirty_dancing.jpg' )
+	};
+
+
 // node globals (keep jslint happy)
-}( exports ));
+}( exports, require ));
