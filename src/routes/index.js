e0b7cd5e91c6e326539f848653bf63a8473d769b
Ben Boyle <benjamins.boyle@gmail.com>
2012-02-12 12:19:38 +1000

    adjustments to VLC launching for OSX

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,7 @@
 (function( console, exports, require ) {
 	'use strict';
 
-	var moviesPath = 'f:/dvd/Movies';
+	var moviesPath = '/Volumes/Scavenger/media/dvd';
 
 
 	// use for home page
@@ -12,6 +12,7 @@
 
 	// list all movies in folder
 	exports.listMovies = function( req, res ) {
+		// TODO ignore .DS_store
 		require( 'fs' ).readdir( moviesPath, function( err, files ) {
 			res.render( 'movie/list', { title: 'Movies', files: files });
 		});
@@ -53,23 +54,36 @@
 	// play a movie
 	exports.playMovie = function( req, res ) {
 		var movie = req.params.movie,
-			media = require( 'path' ).join( moviesPath, movie, 'VIDEO_TS' );
+			media = require( 'path' ).join( moviesPath, movie, 'VIDEO_TS' ),
+			vlc = '/Applications/VLC.app/Contents/MacOS/VLC',
+			vlcArgs = [
+				'--fullscreen', // taskbar still visible, W7 bug only?
+				'--sub-language=en,any',
+				'--no-video-title-show',
+				//'--high-priority', // high performance = priority VLC thread
+				// '--rt-offset=2',
+				'--play-and-exit', // quit when done
+				'file:///' + media
+			];
 
-		console.log( 'vlc', media );
+		console.log( vlc, vlcArgs.join( ' ' ));
 
 		// fire up VLC!
-		require( 'child_process' ).spawn( 'C:/Program Files (x86)/VideoLAN/VLC/vlc.exe', [
-			'--fullscreen', // taskbar still visible, W7 bug only?
-			'--sub-language=en',
-			'--no-video-title-show',
-			'--high-priority', // high performance = priority VLC thread
-			'--play-and-exit', // quit when done
-			'file:///' + media
-		]);
+		// require( 'child_process' ).spawn( 'C:/Program Files (x86)/VideoLAN/VLC/vlc.exe', [
+		vlc = require( 'child_process' ).spawn( vlc, vlcArgs );
 		// VLC OPTIONS
 		// full screen
 		// subtitles on (english)
 		// should these be specified in vulcan, or should the user configure VLC?
+
+		// log
+		// vlc.stdout.on( 'data', function ( data ) {
+		// 	console.log( 'stdout: ' + data );
+		// });
+		// vlc.stderr.on( 'data', function ( data ) {
+		// 	console.log( 'stderr: ' + data );
+		// });
+
 
 		// show "now playing" page
 		res.render( 'movie/play', { title: 'Playing ' + movie });
