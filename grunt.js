3f4a9ae20d0fea67b5f7b3968efb22db4736cc59
Ben Boyle <benjamins.boyle@gmail.com>
2013-01-04 18:04:58 +1000

    added grunt-contrib-watch plugin
    - added watch plugin for grunt (tested on OSX ML)
    - updated package.json (added watch plugin)
    - added /node_modules/ to ignore list

--- grunt.js
+++ grunt.js
@@ -1,40 +1,44 @@
 module.exports = function(grunt) {
 
-  // Project configuration.
-  grunt.initConfig({
-    pkg: '<json:package.json>',
-    test: {
-      files: ['test/**/*.js']
-    },
-    lint: {
-      files: ['grunt.js', 'lib/**/*.js', 'test/**/*.js'],
-      src: ['src/app.js', 'src/routes/*.js', 'src/public/ui/*.js']
-    },
-    watch: {
-      files: '<config:lint.src>',
-      tasks: 'default'
-    },
-    jshint: {
-      options: {
-        curly: true,
-        eqeqeq: true,
-        immed: true,
-        latedef: true,
-        newcap: true,
-        noarg: true,
-        sub: true,
-        undef: true,
-        boss: true,
-        eqnull: true,
-        node: true
-      },
-      globals: {
-        exports: true
-      }
-    }
-  });
+	// plugins
+	grunt.loadNpmTasks('grunt-contrib-watch');
 
-  // Default task.
-  grunt.registerTask('default', 'lint:src');
+
+	// Project configuration.
+	grunt.initConfig({
+	pkg: '<json:package.json>',
+	test: {
+		files: ['test/**/*.js']
+	},
+	lint: {
+		files: ['grunt.js', 'lib/**/*.js', 'test/**/*.js'],
+		src: ['src/app.js', 'src/routes/*.js', 'src/public/ui/*.js']
+	},
+	watch: {
+		files: '<config:lint.src>',
+		tasks: 'default'
+	},
+	jshint: {
+		options: {
+		curly: true,
+		eqeqeq: true,
+		immed: true,
+		latedef: true,
+		newcap: true,
+		noarg: true,
+		sub: true,
+		undef: true,
+		boss: true,
+		eqnull: true,
+		node: true
+		},
+		globals: {
+		exports: true
+		}
+	}
+	});
+
+	// Default task.
+	grunt.registerTask('default', 'lint:src');
 
 };
