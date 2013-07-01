b9141db818d2361a16e8127a7ea6609e1a18d692
Ben Boyle <benjamins.boyle@gmail.com>
2013-07-01 17:19:53 +1000

    updated grunt

---
+++ Gruntfile.js
@@ -0,0 +1,58 @@
+'use strict';
+
+module.exports = function( grunt ) {
+
+	// Project configuration.
+	grunt.initConfig({
+		// Metadata.
+		pkg: grunt.file.readJSON( 'package.json' ),
+		banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
+			'<%= grunt.template.today("yyyy-mm-dd") %>\n' +
+			'<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
+			'* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
+			' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */\n',
+		// Task configuration.
+		clean: {
+			build: [ 'build' ]
+		},
+		// code quality tasks
+		jshint: {
+			gruntfile: {
+				options: {
+					jshintrc: '.jshintrc'
+				},
+				src: 'Gruntfile.js'
+			},
+			src: {
+				options: {
+					jshintrc: '.jshintrc'
+				},
+				src: [
+					'src/*.js',
+					'!src/public/ui/lib/*.js',
+					'src/**/*.json'
+				]
+			},
+		},
+		watch: {
+			gruntfile: {
+				files: '<%= jshint.gruntfile.src %>',
+				tasks: [ 'jshint:gruntfile' ]
+			},
+			srcJs: {
+				files: '<%= jshint.src.src %>',
+				tasks: [ 'jshint:src' ]
+			}
+		}
+	});
+
+	// These plugins provide necessary tasks.
+	grunt.loadNpmTasks( 'grunt-contrib-clean' );
+	grunt.loadNpmTasks( 'grunt-contrib-jshint' );
+	grunt.loadNpmTasks( 'grunt-contrib-watch' );
+
+	// Default task.
+	grunt.registerTask( 'test', [ 'jshint' ]);
+	grunt.registerTask( 'default', [ 'test' ]);
+
+};
